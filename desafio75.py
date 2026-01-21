d16b88be98fb6c956edfc7f76f1495e681b9b115
jessicamarq <jessicamaq@gmail.com>
2026-01-20 23:26:32 -0300

    Addin Ex75

--- desafio75.py
+++ desafio75.py
@@ -3,3 +3,19 @@ A) Quantas vezes apareceu o valor 9.
 B) Em que posição foi digitado o primeiro valor 3.
 C) Quais foram os números pares."""
 
+tupla = (int(input('Digite o 1o valor: ')),
+  int(input('Digite o 2o valor: ')),
+  int(input('Digite o 3o valor: ')),
+  int(input('Digite o 4o valor: ')))
+cont = tupla.count(9)
+print(f'\nO número 9 aparece {cont} vezes')
+if 3 in tupla:
+  posicao = tupla.index(3)
+  print(f'\nO número 3 aparece pela primeira vez na posição {posicao}')
+else:
+  print('O número 3 não foi digitado nenhuma vez')
+
+print('\nOs pares são: ')
+for i in tupla:
+  if int(i) % 2 == 0:
+    print(i, end=' ')
