4b0c42fd916b1b741cf09ac578c8a3709c94e837
jessicamarq <jessicamaq@gmail.com>
2026-01-21 18:50:27 -0300

    Adding Ex76,77

--- desafio76.py
+++ desafio76.py
@@ -1,2 +1,12 @@
 """Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços, na sequência. No final, mostre uma listagem de preços, organizando os dados em forma tabular (alinhando o texto à esquerda e o preço à direita)."""
 
+import pandas as pd
+produtos = [
+  ('Coca', 7.49),
+  ('U.F.O.', 5.49),
+  ('Banana', 5.50),
+  ('Mamão Formosa', 3.50)
+  ] 
+
+tabela = pd.DataFrame(produtos, columns=["Produto", "Preço"])
+print(tabela)
--- desafio77.py
+++ desafio77.py
@@ -1,1 +1,22 @@
 """Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais."""
+
+palavras = (
+  'banana',
+  'computador',
+  'python',
+  'guanabara',
+  'programacao',
+  'desafio',
+  'estudo',
+  'livro',
+  'internet',
+  'teclado'
+)
+vogais = 'aeiou'
+for i in palavras:
+  print(f'Na palavra {i.upper()} temos:', end=' ')
+  lista = []
+  for j in i:
+    if j in vogais:
+      lista.append(j)
+  print('-'.join(lista))
